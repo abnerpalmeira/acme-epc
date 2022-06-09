7926c2b7bb61dd695485939d7da83694b6c00c4f
abner <apalmeira@datakitchen.io>
2022-06-09 11:46:58 -0300

    feat: add regex for input validation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import re
+
+r = re.compile("([a-zA-Z]+)=(([a-zA-Z]+(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9](,|$))+)")
 with open('input.txt', 'r') as f:
     data = f.readlines()
     for line in data:
-        print(line)
+        x = r.match(line)
+        print(x)
